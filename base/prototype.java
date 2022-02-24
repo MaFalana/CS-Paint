7a6633ef97ecbead8d259899ec8969484b45f9a1
Falana, Malik Omolade <mfalana@iu.edu>
2022-02-24 11:10:55 -0500

    Update prototype.java

--- base/prototype.java
+++ base/prototype.java
@@ -1,1 +1,14 @@
+/*
+Program: Color Picker
+Written By: Malik Falana
+Date: 02/24/2022
+Purpose: To Create a color picker gui
+*/
 
+import javax.swing.*;
+
+public class colorPicker
+{
+  private JButton Upload;
+  
+}
